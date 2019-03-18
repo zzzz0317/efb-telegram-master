4e054f6501c8d3bf0aa3ddd289cb4d5336db5c26
Zhe Zhang <zhangzhe0177@gmail.com>
2019-03-18 08:53:38 +0800

    Update slave_message.py

--- efb_telegram_master/slave_message.py
+++ efb_telegram_master/slave_message.py
@@ -627,9 +627,9 @@ class SlaveMessageProcessor(LocaleMixin):
             name_prefix = ETMChat(chat=msg.chat, db=self.db).display_name
             if msg.chat != msg.author:
                 name_prefix += ", %s" % ETMChat(chat=msg.author, db=self.db).display_name
-                loggertxt = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ' - Sent a message to ' + ETMChat(chat=msg.chat, db=self.db).display_name
+                loggertxt = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ' - ' + str(msg.author) + 'sent a message to ' + ETMChat(chat=msg.chat, db=self.db).display_name
                 f = open('/var/zzlogger/efblog.txt', 'a')
-                f.write(str(loggertxt.encode('utf-8').strip()+b"\n"))
+                f.write(str(loggertxt.encode("utf-8")) + "\n")
                 f.close()
             msg_template = "%s %s:" % (emoji_prefix, name_prefix)
         elif msg.chat.chat_type == ChatType.Group:
